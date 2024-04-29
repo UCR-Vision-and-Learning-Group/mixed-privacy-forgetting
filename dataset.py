b18520c22ea3b7573da6581095fd4b3bbd8e7886
umityigitbsrn <umityigitbsrn@gmail.com>
2024-04-29 14:51:06 -0700

    core train split, core train

--- dataset.py
+++ dataset.py
@@ -1,6 +1,66 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
 
-def split_dataset_core_train():
-    pass
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize, Lambda
+
+import numpy as np
+
+
+def get_data_transformations(dataset_id, arch_id, one_hot=True):
+    transform, target_transform = None, None
+    if dataset_id == 'cifar10' and arch_id == 'resnet18':
+        transform = Compose([
+            Resize((224, 224)),  # Resize to match ResNet input size
+            ToTensor(),
+            Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))  # Normalize
+        ])
+
+        if one_hot:
+            target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+    
+    return transform, target_transform
+
+
+class SubsetToDataset(Dataset):
+    def __init__(self, subset, transform=None, target_transform=None):
+        self.subset = subset
+        self.transform = transform
+        self.target_transform = target_transform
+        
+    def __getitem__(self, index):
+        x, y = self.subset[index]
+        if self.transform:
+            x = self.transform(x)
+
+        if self.target_transform:
+            y = self.target_transform(y)
+
+        return x, y
+        
+    def __len__(self):
+        return len(self.subset)
+
+
+def split_dataset_core_train(dataset_id, arch_id, split_rate):
+    whole_dataset, transform, target_transform = None, None, None
+    if dataset_id == 'cifar10':
+        whole_dataset = CIFAR10('./data', train=True, transform=None, target_transform=None, download=True)
+        transform, target_transform = get_data_transformations(dataset_id, arch_id)
+    
+    len_train_dataset = int(len(whole_dataset) * split_rate)
+    len_core_dataset = len(whole_dataset) - len_train_dataset
+    core_subset, train_subset = random_split(whole_dataset, [len_core_dataset, len_train_dataset])    
+
+    core_dataset = SubsetToDataset(core_subset, transform=transform, target_transform=None) # ce loss
+    train_dataset = SubsetToDataset(train_subset, transform=transform, target_transform=target_transform) # mse loss
+    return core_dataset, train_dataset
+
+def get_core_train_loader(core_dataset, train_dataset, batch_size, shuffle=True):
+    core_loader = DataLoader(core_dataset, batch_size=batch_size, shuffle=shuffle)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    return core_loader, train_loader
+
 
 def split_train_dataset_remaining_forget():
     pass
--- train.py
+++ train.py
@@ -1,5 +1,74 @@
-def train_core_dataset():
-    pass
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+
+from torchvision.models import resnet18, ResNet18_Weights
+
+
+def train_core_dataset(core_loader, arch_id, criterion_id, optimizer_id, lr, num_epoch,
+                       save_path=None, device_id=0, use_pretrained=True):
+    device = 'cuda:{}'.format(device_id) if torch.cuda.is_available() else 'cpu'
+    
+    # init model
+    model = None
+    if arch_id == 'resnet18':
+        if use_pretrained:
+            model = resnet18(weights=ResNet18_Weights)
+        else:
+            model = resnet18()
+    model = model.to(device)
+
+    # init criterion
+    criterion = None
+    if criterion_id == 'ce':
+        criterion = nn.CrossEntropyLoss()
+    
+    # init optimizer
+    optimizer = None
+    if optimizer_id == 'adam':
+        optimizer = Adam(model.parameters(), lr=lr)
+    
+    # train
+    running_loss = []
+    running_acc = []
+    for epoch in num_epoch:
+        model.train()
+        for iter_idx, (core_data, core_label) in enumerate(core_loader):
+            core_data, core_label = core_data.to(device), core_label.to(device)
+
+            optimizer.zero_grad()
+            preds = model(core_data)
+            loss = criterion(preds, core_label)
+            loss.backward()
+            optimizer.step()
+            running_loss.append(loss.item())
+
+            if iter_idx == 0 or (iter_idx + 1) % 100 == 0 or (iter_idx + 1) == len(core_loader):
+                print('#########epoch: {}, iter: {}, loss: {}#########'.format(epoch + 1, iter_idx + 1, loss.item())) 
+
+        model.eval()
+        with torch.no_grad():
+            true_count = 0
+            sample_count = 0
+            for core_data, core_label in enumerate(core_loader):
+                core_data, core_label = core_data.to(device), core_label.to(device)
+                preds = model(core_data)
+                
+                predicted_label = torch.argmax(preds, dim=1)
+                true_count += torch.count_nonzero(predicted_label == core_label).item()
+                sample_count += core_data.shape[0]
+
+            print('=========epoch: {}, accuracy: {}========='.format(epoch + 1, (true_count / sample_count)))
+            running_acc.append((true_count / sample_count))
+    
+        if save_path:
+            torch.save({
+                'epoch': epoch, 
+                'running_loss': running_loss,
+                'running_acc': running_acc,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, save_path)
 
 def train_train_dataset():
     pass
