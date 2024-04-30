36200859c2da8ab6114c24e458c22022a9f8296e
umityigitbsrn <umityigitbsrn@gmail.com>
2024-04-29 23:57:54 -0700

    jvp implemented

--- scipts.sh
+++ scipts.sh
@@ -10,3 +10,20 @@ python train.py --mode train-core-dataset --dataset-id mnist --arch-id simple \
 --learning-rate 0.001 --num-epoch 20 --shuffle \
 --save-path ./checkpoints/042924-train-core-dataset-mnist/last_checkpoint.pth \
 --reshape-data -1 784
+
+
+# train with jvp train dataset -- mnist
+python train.py --mode train-with-jvp-train-dataset --dataset-id mnist --arch-id simple \
+--checkpoint-path ./checkpoints/042924-train-core-dataset-mnist/last_checkpoint.pth \
+--split-rate 0.2 --batch-size 128 --criterion-id mse --optimizer-id adam \
+--learning-rate 0.001 --num-epoch 100 --shuffle \
+--save-path ./checkpoints/042924-train-with-jvp-train-dataset-mnist/last_checkpoint.pth \
+--reshape-data -1 784 --device-id 1 --weight-decay 0.001
+
+# train with jvp train dataset -- cifar10
+python train.py --mode train-with-jvp-train-dataset --dataset-id cifar10 --arch-id resnet18 \
+--checkpoint-path ./checkpoints/042924-train-core-dataset-cifar10/last_checkpoint.pth \
+--split-rate 0.2 --batch-size 128 --criterion-id mse --optimizer-id adam \
+--learning-rate 0.001 --num-epoch 100 --shuffle \
+--save-path ./checkpoints/042924-train-with-jvp-train-dataset-cifar10/last_checkpoint.pth \
+--device-id 0 --weight-decay 0.001
--- train.py
+++ train.py
@@ -12,7 +12,8 @@ from model import SimpleNet, MixedLinearModel
 
 
 def train_core_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer_id, lr, num_epoch,
-                       save_path=None, device_id=0, use_pretrained=True, print_loss_log=10, reshape_data=None):
+                       save_path=None, device_id=0, use_pretrained=True, print_loss_log=10, reshape_data=None,
+                       weight_decay=0):
     device = 'cuda:{}'.format(device_id) if torch.cuda.is_available() else 'cpu'
     
     # init model
@@ -26,7 +27,7 @@ def train_core_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer
         if dataset_id == 'cifar10':
             num_ftrs = model.fc.in_features
             model.fc = nn.Linear(num_ftrs, 10)
-            
+
     elif arch_id == 'simple':
         model = SimpleNet(784, 10)
 
@@ -40,7 +41,7 @@ def train_core_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer
     # init optimizer
     optimizer = None
     if optimizer_id == 'adam':
-        optimizer = Adam(model.parameters(), lr=lr)
+        optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     
     # train
     running_loss = []
@@ -70,8 +71,11 @@ def train_core_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer
                 if reshape_data:
                     core_data = core_data.reshape(tuple(reshape_data))
                 preds = model(core_data)
-                
-                predicted_label = torch.argmax(preds, dim=1)
+
+                predicted_label = torch.argmax(preds, dim=1)  
+                if len(train_label.shape) > 2:
+                    train_label = torch.argmax(train_label, dim=1)
+
                 true_count += torch.count_nonzero(predicted_label == core_label).item()
                 sample_count += core_data.shape[0]
 
@@ -91,8 +95,8 @@ def train_core_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer
             }, save_path)
             
 
-def train_with_jvp_train_dataset(core_loader, dataset_id, arch_id, criterion_id, optimizer_id, lr, num_epoch, 
-                                 save_path=None, device_id=0, use_pretrained=True, print_loss_log=10, reshape_data=None):
+def train_with_jvp_train_dataset(train_loader, dataset_id, arch_id, checkpoint_path, criterion_id, optimizer_id, lr, num_epoch, 
+                                 save_path=None, device_id=0, print_loss_log=10, reshape_data=None, weight_decay=0):
     device = 'cuda:{}'.format(device_id) if torch.cuda.is_available() else 'cpu'
 
     # save core model parameters
@@ -104,6 +108,76 @@ def train_with_jvp_train_dataset(core_loader, dataset_id, arch_id, criterion_id,
             model.fc = nn.Linear(num_ftrs, 10)
     elif arch_id == 'simple':
         model = SimpleNet(784, 10)
+
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['model'])
+    params = {name: p.detach().clone().to(device) for name, p in model.named_parameters()}
+    del model
+
+    mixed_linear = MixedLinearModel(dataset_id, arch_id)
+    mixed_linear = mixed_linear.to(device)
+
+    # init criterion
+    criterion = None
+    if criterion_id == 'ce':
+        criterion = nn.CrossEntropyLoss()
+    elif criterion_id == 'mse':
+        criterion = nn.MSELoss()
+    
+    # init optimizer
+    optimizer = None
+    if optimizer_id == 'adam':
+        optimizer = Adam(mixed_linear.parameters(), lr=lr, weight_decay=weight_decay)
+    
+    # train
+    running_loss = []
+    running_acc = []
+    for epoch in range(num_epoch):
+        mixed_linear.train()
+        for iter_idx, (train_data, train_label) in enumerate(train_loader):
+            train_data, train_label = train_data.to(device), train_label.to(device)
+            if reshape_data:
+                train_data = train_data.reshape(tuple(reshape_data))
+            optimizer.zero_grad()
+            preds = mixed_linear(params, train_data)
+            loss = criterion(preds, train_label)
+            loss.backward()
+            optimizer.step()
+            running_loss.append(loss.item())
+
+            if iter_idx == 0 or (iter_idx + 1) % print_loss_log == 0 or (iter_idx + 1) == len(train_loader):
+                print('#########epoch: {}, iter: {}, loss: {}#########'.format(epoch + 1, iter_idx + 1, loss.item())) 
+
+        mixed_linear.eval()
+        with torch.no_grad():
+            true_count = 0
+            sample_count = 0
+            for train_data, train_label in train_loader:
+                train_data, train_label = train_data.to(device), train_label.to(device)
+                if reshape_data:
+                    train_data = train_data.reshape(tuple(reshape_data))
+                preds = mixed_linear(params, train_data)
+                
+                predicted_label = torch.argmax(preds, dim=1)
+                if len(train_label.shape) == 2:
+                    train_label = torch.argmax(train_label, dim=1)
+                true_count += torch.count_nonzero(predicted_label == train_label).item()
+                sample_count += train_data.shape[0]
+
+            print('=========epoch: {}, accuracy: {}========='.format(epoch + 1, (true_count / sample_count)))
+            running_acc.append((true_count / sample_count))
+    
+        if save_path:
+            if not os.path.exists(os.path.split(save_path)[0]):
+                os.makedirs(os.path.split(save_path)[0])
+
+            torch.save({
+                'epoch': epoch, 
+                'running_loss': running_loss,
+                'running_acc': running_acc,
+                'model': mixed_linear.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, save_path)
 
 
 
@@ -129,6 +203,9 @@ if __name__ == "__main__":
     parser.add_argument('-rd', '--reshape-data', dest='reshape_data', nargs='*', type=int)
     parser.add_argument('-se', '--seed', dest='seed', type=int, default=13)
 
+    parser.add_argument('-cp', '--checkpoint-path', dest='checkpoint_path', type=str)
+    parser.add_argument('-wd', '--weight-decay', dest='weight_decay', type=float, default=0)
+
     args = parser.parse_args()
 
     if args.mode == 'train-core-dataset':
@@ -137,3 +214,10 @@ if __name__ == "__main__":
         train_core_dataset(core_loader, args.dataset_id, args.arch_id, args.criterion_id, args.optimizer_id,
                            args.learning_rate, args.num_epoch, save_path=args.save_path, device_id=args.device_id,
                            use_pretrained=args.use_pretrained, print_loss_log=args.print_loss_log, reshape_data=args.reshape_data)
+    elif args.mode == 'train-with-jvp-train-dataset':
+        _, train_dataset = split_dataset_core_train(args.dataset_id, args.arch_id, args.split_rate, seed=args.seed)
+        _, train_loader = get_core_train_loader(_, train_dataset, args.batch_size, shuffle=args.shuffle)
+        train_with_jvp_train_dataset(train_loader, args.dataset_id, args.arch_id, args.checkpoint_path, 
+                                     args.criterion_id, args.optimizer_id, args.learning_rate, args.num_epoch,
+                                     save_path=args.save_path, device_id=args.device_id, print_loss_log=args.print_loss_log,
+                                     reshape_data=args.reshape_data, weight_decay=args.weight_decay)
